865dd8d121a93e8b3991de4cde12c323f8be5e24
Qu4k <pippo.reds@gmail.com>
2020-07-30 16:36:42 +0200

    chore: add head tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Developing cool deno.land modules, such as denon"
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
